d2d5cfc30dbe982747aced9603a42d91deaffef8
Onetyten <onetyten@gmail.com>
2025-10-22 18:54:42 +0100

    hide scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <Router>
             <div className="flex bg-background text-white font-console items-center w-screen min-h-dvh overflow-hidden relative">
               <Sidebar pages = {pages} setPages = {setPages} />
-              <div className="flex flex-col flex-1 p-4 sm:p-8  overflow-y-scroll justify-start items-center h-dvh ">
+              <div className="flex flex-col flex-1 p-4 sm:p-8  overflow-y-scroll hide-scrollbar justify-start items-center h-dvh ">
                 <div className="flex flex-col w-full gap-6 ">
                   <SearchBar/>
                   <Routes>
